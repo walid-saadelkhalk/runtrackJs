48138ded919c11fa50d6badca8f9d238a604ca4f
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-18 16:53:56 +0200

    [CREATE] day 4 job 2 done

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,7 +1,14 @@
-document.getElementById('button').addEventListener('click', () => {
-    fetch('expression.txt')
-      .then(response => response.ok ? response.text() : Promise.reject('Erreur de récupération du fichier.'))
-      .then(data => document.getElementById('content').innerHTML = `<p>${data}</p>`)
-      .catch(error => console.error('Une erreur s\'est produite :', error));
+document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', () => {
+      const key = button.dataset.key;
+      fetch('doc.json')
+          .then(response => response.json())
+          .then(data => displayData(data[key]))
+          .catch(error => console.error('Error fetching data:', error));
   });
-  
+});
+
+function displayData(data) {
+  document.getElementById('content').innerText = data;
+}
+
